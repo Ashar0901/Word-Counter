27a06d8aa1133d5f9e58b17e2f272061ba6dc980
Ashar0901 <159641956+Ashar0901@users.noreply.github.com>
2024-04-28 13:28:26 -0700

    Push Word Counter on Github

---
+++ index.js
@@ -0,0 +1,6 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([{ message: "Please enter your sentence", type: "input", name: "sentence" }]);
+let word = answer.sentence.trim().split(" ");
+console.log(word);
+console.log(`"Your Word count is ${word.length}"`);
